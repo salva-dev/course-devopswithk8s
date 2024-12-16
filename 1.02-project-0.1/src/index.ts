fb5888faad6b254ed8dd08c22f12a1fb3e344aa1
Salvador Santander Gutiérrez <salvador.santander@juntadeandalucia.es>
2024-12-16 13:13:07 +0100

    feat: exercise 1.02: Add node code

---
+++ 1.02-project-0.1/src/index.ts
@@ -0,0 +1,14 @@
+// src/index.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Server started in port ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
